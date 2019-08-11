aba95992599b8ab100ac2c99fb6bdba8885576ee
Lauren Dove <elledoveinc@gmail.com>
2019-08-11 01:08:45 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,13 @@
 first_number = 10
+
 second_number = 25
+
 sum = first_number + second_number
+
 difference = second_number - first_number
-product first_number * second_number
-quotient = second_number / first_number# Add your variables here 
+
+product = first_number * second_number
+
+quotient = second_number / first_number
+
+# Add your variables here 
